a6f651c498142f5c01d99a19bc57e356ccc8bb3d
Aditya Singh <as1671109@gmail.com>
2024-07-08 13:23:03 +0530

    commit

--- linked_list/src/SinglyLinkedList.java
+++ linked_list/src/SinglyLinkedList.java
@@ -77,9 +77,100 @@ public class SinglyLinkedList {
         return temp;
     }
 
-    // public ListNode deleteLast(){
-    //     if(head==null) return null;
-    // }
+    public ListNode deleteLast(){
+        if(head==null || head.next == null) return head;
+        ListNode current = head;
+        ListNode previous = null;
+        while (current.next != null) {
+            previous = current;
+            current = current.next;
+        }
+        previous.next = null;
+        return current;
+    }
+
+    public void deleteFromSpecificPosition(int position){
+        if (position == 1) {
+            head = head.next;
+        } else {
+            ListNode previous = head;
+            int count = 1;
+            while (count<position - 1) {
+                previous = previous.next;
+                count++;
+            }
+            ListNode current = previous.next;
+            previous.next = current.next;
+        }        
+        
+    }
+    public void deleteByKey(int key){
+        ListNode current = head;
+        ListNode temp = null;
+        if (current != null && current.data == key) {
+            head = current.next;
+            return;
+        }
+        while (current != null && current.data!= key) {
+            temp = current;
+            current = current.next;
+        }
+        if (current==null) {
+            return;
+        }
+        temp.next = current.next;
+    }
+    
+    public boolean containsLoop(){
+        ListNode fastPtr = head;
+        ListNode slowPtr = head;
+        while (fastPtr!=null && fastPtr.next != null) {
+            fastPtr = fastPtr.next.next;
+            slowPtr = slowPtr.next;
+            if (slowPtr==fastPtr) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void createALoopInLinkedList(){
+        ListNode first = new ListNode(1);
+        ListNode second = new ListNode(2);
+        ListNode third = new ListNode(3);
+        ListNode fourth = new ListNode(4);
+        ListNode fifth = new ListNode(5);
+        ListNode sixth = new ListNode(6);
+        head = first;
+        first.next = second;
+        second.next = third;
+        third.next = fourth;
+        fourth.next = fifth;
+        sixth.next = third;
+        
+    }
+
+    public ListNode startNodeInALoop(){
+        ListNode fastPtr = head;
+        ListNode slowPtr = head;
+        while (fastPtr!=null && fastPtr.next != null) {
+            fastPtr = fastPtr.next.next;
+            slowPtr = slowPtr.next;
+            if (slowPtr==fastPtr) {
+                return getStartingNode(slowPtr);
+            }
+        }
+        return null;
+    }
+
+    private SinglyLinkedList.ListNode getStartingNode(SinglyLinkedList.ListNode slowPtr) {
+        ListNode temp = head;
+        while (temp != head) {
+            temp = temp.next;
+            slowPtr = slowPtr.next;
+        }
+        return temp;
+    }
 
     public static void main(String[] args) {
         SinglyLinkedList sll = new SinglyLinkedList();
@@ -106,5 +197,17 @@ public class SinglyLinkedList {
         
         sll.deleteFirst();
         sll.printAllElemets();
+        System.out.println();
+        
+        sll.deleteLast();
+        sll.printAllElemets();
+        System.out.println();
+
+        sll.deleteFromSpecificPosition(2);
+        sll.printAllElemets();
+        System.out.println();
+
+        sll.createALoopInLinkedList();
+        System.out.println(sll.containsLoop());
     }
 }
